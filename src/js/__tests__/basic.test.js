35d987c0ad03be6162b1b10b570099ccded3c686
DmitriyBaev <100maverick100@gmail.com>
2021-07-26 19:46:30 +0300

    add toBe in test

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,17 +1,21 @@
 import sortUnits from '../basic';
 
+const result = sortUnits([
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+]);
+
+const expectation = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
 test('sortedHealth', () => {
-  const result = sortUnits([
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ]);
-
-  const expectation = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-
   expect(result).toEqual(expectation);
+});
+
+test('sortedHealth2', () => {
+  expect(result).not.toBe(expectation);
 });
